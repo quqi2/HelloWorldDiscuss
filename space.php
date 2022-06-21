f94948ac745fd08aa0f2b6c041512d6e21f40668
quqi2 <46997899+quqi2@users.noreply.github.com>
2022-06-21 11:29:49 +0800

    Update space.php

--- space.php
+++ space.php
@@ -1,3 +1,20 @@
 <?php
-//还在写，这是个人空间
+ $get=$_GET;
+ $config = json_decode(file_get_contents('config.json'), true);
+ $conn = new mysqli(
+        $config["database.host"],
+        $config["database.user"],
+        $config["database.pass"],
+        $config["database.name"]
+    );
+ if( is_null($get["uid"]) ){
+    echo "<script>alert('输入的uid为空')</script>";
+    echo "<meta http-equiv=\"Refresh\" content=\"0;url=index.php\">";
+    die("1");
+ }
+ if( is_numeric($get["uid"]) == false ){
+    echo "<script>alert('输入的参数必须是数字')</script>";
+    echo "<meta http-equiv=\"Refresh\" content=\"0;url=index.php\">";
+    die("1");
+ }
 ?>
